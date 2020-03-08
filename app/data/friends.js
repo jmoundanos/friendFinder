33b9d80bdf7706b1550fc1f276fae76df7edb077
Jennifer Moundanos <jmoundanos@gmail.com>
2020-03-08 17:09:53 -0500

    add api logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
-var friends= 
+var friends= [
 {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://www.pexels.com/photo/adult-beard-boy-casual-220453/",
     "scores":[
         5,
         1,
@@ -14,4 +14,117 @@ var friends=
         4,
         1
       ]
+  },
+  {
+  "name": "Jacob Deming",
+  "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+  "scores": [
+  "4",
+  "2",
+  "5",
+  "1",
+  "3",
+  "2",
+  "2",
+  "1",
+  "3",
+  "2"
+  ]
+  },
+  {
+  "name": "Jeremiah Scanlon",
+  "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+  "scores": [
+  "5",
+  "2",
+  "2",
+  "2",
+  "4",
+  "1",
+  "3",
+  "2",
+  "5",
+  "5"
+  ]
+  },
+  {
+  "name": "Louis T. Delia",
+  "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+  "scores": [
+  "3",
+  "3",
+  "4",
+  "2",
+  "2",
+  "1",
+  "3",
+  "2",
+  "2",
+  "3"
+  ]
+  },
+  {
+  "name": "Lou Ritter",
+  "photo": "https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/",
+  "scores": [
+  "4",
+  "3",
+  "4",
+  "1",
+  "5",
+  "2",
+  "5",
+  "3",
+  "1",
+  "4"
+  ]
+  },
+  {
+  "name": "Jordan Biason",
+  "photo": "https://www.google.com/url?sa=i&url=http%3A%2F%2Fwww.freedigitalphotos.net%2Fimages%2Fpeople_g40.html&psig=AOvVaw1FJV60qPI5Xt6DpLQFEX5W&ust=1583722419415000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCfy_XviegCFQAAAAAdAAAAABAK",
+  "scores": [
+  "4",
+  "4",
+  "2",
+  "3",
+  "2",
+  "2",
+  "3",
+  "2",
+  "4",
+  "5"
+  ]
+  },
+  {
+  "name": "Bob",
+  "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.biography.com%2Fartist%2Fbob-ross&psig=AOvVaw2u5zSrFdDkpxDEQlprOuq7&ust=1583602381727000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNji5eOwhugCFQAAAAAdAAAAABAD",
+  "scores": [
+  "5",
+  "3",
+  "2",
+  "1",
+  "2",
+  "1",
+  "3",
+  "3",
+  "5",
+  "2"
+  ]
+  },
+  {
+  "name": "Bob",
+  "photo": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.biography.com%2Fartist%2Fbob-ross&psig=AOvVaw2u5zSrFdDkpxDEQlprOuq7&ust=1583602381727000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNji5eOwhugCFQAAAAAdAAAAABAD",
+  "scores": [
+  "5",
+  "3",
+  "2",
+  "1",
+  "2",
+  "1",
+  "3",
+  "3",
+  "5",
+  "2"
+  ]
   }
+]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,32 @@ module.exports = function(app) {
 	});
 
 	app.post("/api/friends", function(req, res) {
-		return res.json(friendData);
-	});
+        //get friend scores from friends.js
+        var friendScores = req.body.scores;
+        //make new array to send the compared scores to
+        var scoresArray = [];
+        //set variable for best match
+        var bestFriend = 0;
+
+        //iterate through friends data
+        for(var i=0; i<friendData.length; i++){
+            var diffScores = 0;
+            //compare scores
+            for(var j=0; j<friendScores.length; j++){
+                diffScores += (Mat.abs(parseInt(friendData[i].scores[j])- parseInt(friendScore[j])));
+            }
+            //push scores into array
+            scoresArray.push(diffScores);
+        }
+        //find best friend match
+        for(var i=0; i<scoresArray.length; i++){
+            if(scoresArray[i] <= scoresArray[bestFriend]){
+                bestFriend = i;
+            }
+        }
+        //return best friend
+        res.json(friendData[bestFriend]);
+        friendData.push(req.body);
+    });
+    
 };
